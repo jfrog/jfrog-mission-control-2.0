1e188c075cc2ea51e4f57077c7d4fb7f828f96e5
daviekimxu <davidx@davidx-mac.inc.jfrog.local>
2017-11-08 17:09:52 -0800

    1st draft
    Davids scripts

---
+++ replicationscript.groovy
@@ -0,0 +1,74 @@
+//Artifactory
+art1 = userInput (
+    type : "ARTIFACTORY",
+    description : "Please provide the target Artifactory you want to host the replication",
+
+    )
+
+repokey = userInput (
+    type : "STRING",
+    description : "Please provide the target Repository you want to host the replication",
+
+    )
+
+
+replicationType  = userInput (
+    type : "STRING",
+    description : "Please specify push or pull replication",
+    )
+
+replicationUrl = userInput (
+    type : "STRING",
+    description : "For pull, specify the URL of the repository you want to Proxy. For push, speicify target repository URL",
+
+    )
+
+eventBased = userInput(
+	type : "BOOLEAN",
+	description : "event based replication?" 
+
+    )
+
+
+if (replicationType == "pull")(
+
+		
+		artifactory(art1.name){
+        	remoteRepository(repokey){
+            	
+
+            replication (replicationUrl){
+     				  cronExp "0 0/9 14 * * ?"
+      				socketTimeoutMillis 15000
+      				username "remote-repo-user"
+      				password "pass"
+      				enableEventReplication eventBased
+      				enabled true
+      				syncDeletes false
+      				syncProperties true
+      				clientTlsCertificate ""
+
+        		}
+        }
+}
+)
+
+else if (replicationType == "push")(
+		artifactory(art1.name) { 
+        	localRepository(repokey) {
+        		
+  				replication(replicationUrl) {
+      				cronExp "0 0/9 14 * * ?"
+     		 		  socketTimeoutMillis 15000
+      				username "remote-repo-user"
+      				password "pass"
+              enableEventReplication eventBased
+      				enabled true
+      				syncDeletes false
+      				syncProperties true
+      				clientTlsCertificate ""
+
+				}
+    			}
+			}
+	)
---
+++ reposcript.groovy
@@ -0,0 +1,88 @@
+//Artifactory
+art1 = userInput (
+    type : "ARTIFACTORY",
+    description : "Artifactory instance name",
+
+    )
+
+//repokey
+repokeyArt1 = userInput (
+    type : "STRING",
+    description : "Repository Key",
+ 
+  )
+
+//repo type
+repotype = userInput (
+    type : "STRING",
+    description : "Repository Type",
+ 
+  )
+
+//packagetype
+packagetype = userInput (
+    type : "PACKAGE_TYPE", 
+    description : "Package Type",
+
+  )
+
+remoteUrl = userInput (
+    type : "STRING",
+    description : "Enter remote URL (for remotes only)",
+    )
+
+repolist = userInput (
+       type : "STRING",
+        description : "Enter repository keys separated by commas",
+    )
+
+includedrepos = repolist.split(",")*.trim()
+
+if (repotype == "local")(
+    artifactory(art1.name) { 
+        localRepository(repokeyArt1) {
+        description "Public description"
+        notes "Some internal notes"
+        archiveBrowsingEnabled false
+        packageType packagetype
+        }
+    }
+)
+else if (repotype == "remote")(
+
+
+    artifactory(art1.name){
+        remoteRepository(repokeyArt1){
+            url remoteUrl
+            username "remote-repo-user"
+            password "pass"
+            description "Public description"
+            notes "Some internal notes"
+            packageType packagetype
+            remoteRepoChecksumPolicyType "generate-if-absent" // default | "fail" | "ignore-and-generate" | "pass-thru"
+            xrayIndex false
+            blockXrayUnscannedArtifacts false
+            enableFileListsIndexing ""
+        }
+
+    }
+    )
+else if (repotype == "virtual")(
+
+
+
+    artifactory(art1.name) {
+        virtualRepository(repokeyArt1){
+            repositories includedrepos
+            description "Public description"
+            notes "Some internal notes"
+            packageType packagetype
+            debianTrivialLayout false
+            artifactoryRequestsCanRetrieveRemoteArtifacts false
+            pomRepositoryReferencesCleanupPolicy "discard_active_reference" // default | "discard_any_reference" | "nothing"
+        }
+
+    }
+    )
+
+
