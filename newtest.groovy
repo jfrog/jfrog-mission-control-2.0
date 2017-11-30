49fec817346ab528f06f4e27ae7cd4c9398c6394
mission-control <mission-control@5edd6bdf995c>
2017-11-30 22:24:53 +0000

    File changed by admin

--- newtest.groovy
+++ newtest.groovy
@@ -1,4 +1,10 @@
-artifactory($whichArtifactory){
+whichArtifactory = userInput (
+    type : "ARTIFACTORY",
+    name : "Which Artifactory",
+    description : "Please specify the Artifactory service on which to create the repository"
+  )
+
+artifactory(whichArtifactory.name){
    localRepository("docker-temp") {
      packageType "docker"
      description "My local Docker registry"
