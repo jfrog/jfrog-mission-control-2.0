28abd46eb22db16ee5aa6ad92c2f1aa8da3de239
mission-control <mission-control@5edd6bdf995c>
2017-11-30 22:15:13 +0000

    File changed by admin

---
+++ newtest.groovy
@@ -0,0 +1,5 @@
+whichArtifactory = userInput (
+    type : "ARTIFACTORY",
+    name : "Which Artifactory",
+    description : "Please specify the Artifactory service on which to create the repository"
+  )
